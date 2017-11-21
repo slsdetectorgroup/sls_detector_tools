1b8f16601c2693b56c1336cbf4b6f79d5b60ee3f
Erik Frojdh <erik.frojdh@gmail.com>
2017-11-21 16:02:54 +0100

    fixed keithley for python3

--- sls_detector_tools/keithley.py
+++ sls_detector_tools/keithley.py
@@ -8,7 +8,7 @@ import time
 import logging
 logger = logging.getLogger()
 
-import sls_detector.config as cfg
+
 class SourceMeter():
     """
     Class to control a Keithley 2410 SourceMeeter over serial
@@ -58,25 +58,25 @@ class SourceMeter():
         self.serial.write(':FORM:ELEM VOLT,CURR,TIME\n')
        
     def read(self):
-        self.serial.write(':READ?\n')
+        self.serial.write(b':READ?\n')
         m=self.serial.readlines()
         return m
         
     def get_digits(self):
-        self.serial.write(':SENSE:CURR:DIG?\n')
+        self.serial.write(b':SENSE:CURR:DIG?\n')
         m=self.serial.readlines()
         return m
     def set_digits(self, n):
-        self.serial.write(':SENSE:CURR:DIG '+str(n)+'\n')
+        self.serial.write(b':SENSE:CURR:DIG '+str(n)+'\n')
      
     def on(self):
-        self.serial.write('output on\n')
+        self.serial.write(b'output on\n')
     def off(self):
-        self.serial.write('output off\n')
+        self.serial.write(b'output off\n')
         
     def set_voltage(self,V):
-        s='SOUR:VOLT:LEV '+str(V)+'\n'
-        self.serial.write(s)
+        s='SOUR:VOLT:LEV {:d}\n'.format(V)
+        self.serial.write(bytes(s, 'utf-8'))
 
     def remote(self, flag):
         """
@@ -85,8 +85,8 @@ class SourceMeter():
         panel
         """
         if flag:
-            self.serial.write( 'syst:rem\n')
+            self.serial.write( b'syst:rem\n')
         else:
-            self.serial.write( 'syst:loc\n')
+            self.serial.write( b'syst:loc\n')
 
 
