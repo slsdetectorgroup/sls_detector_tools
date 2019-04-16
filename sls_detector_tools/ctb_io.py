9fec592b650bab1a11a192fbeb9fddc68ff91dc0
Erik Frojdh <erik.frojdh@gmail.com>
2019-04-16 17:10:32 +0200

    faster reading

--- sls_detector_tools/ctb_io.py
+++ sls_detector_tools/ctb_io.py
@@ -26,41 +26,33 @@ header_dt = [('frameNumber',np.uint64),
              ('version', np.uint8)]
 
 
-def ExtractBits(data, dr=24, bit_nr0 = 17, bit_nr1 = 6):
-    bit_nr0 = np.uint64(bit_nr0)
-    bit_nr1 = np.uint64(bit_nr1)
-    mask0 = np.uint64(1) << bit_nr0
-    mask1 = np.uint64(1) << bit_nr1
-    counters = np.zeros(n_counters, dtype = np.uint64)
+
+
+def ExtractBits(raw_data, dr=24, bits = (17,6)):
+    bits = np.uint64(bits)
+    data = np.zeros(0, dtype = np.uint64)
+    for bit in bits:
+        tmp = (raw_data >> bit) & np.uint64(1)
+        data = np.hstack((data, tmp))
     
-    data_it = np.nditer(data, op_flags=['readwrite'])
-    counter_it0 = np.nditer(counters, op_flags=['readwrite'])
-    counter_it1 = np.nditer(counters[96:], op_flags=['readwrite'])
-    c=0
-    while not(counter_it1.finished):
-        for i in np.arange(dr, dtype = np.uint64):
-#            print(f'{data_it.value:064b}')
-            tmp0 = np.uint64((data_it.value  & mask0) >> bit_nr0)
-            tmp1 = np.uint64((data_it.value  & mask1) >> bit_nr1)
-            counter_it0[0] = counter_it0[0] | (tmp0 << i)
-            counter_it1[0] = counter_it1[0] | (tmp1 << i)
-            data_it.iternext()
-#        print('\n')
-        counter_it0.iternext()
-        counter_it1.iternext()
+    #Shift the bits to the righ place
+    for i in np.arange(dr, dtype = np.uint64):
+        data[i::dr] = data[i::dr] << i
+
+    data = data.reshape(data.size//dr, dr)
+    return data.sum(axis = 1)
 
 
-    return counters
 
 
-def read_my302_file(fname, dr=24, bit_nr0 = 17, bit_nr1 = 6, 
+def read_my302_file(fname, dr=24, bits = (17,6),
                     offset=0, tail = 72, n_frames=1):
     header = np.zeros(n_frames, header_dt)
-    data = np.zeros((n_frames, 192), dtype = np.uint64)
+    data = np.zeros((n_frames, n_counters), dtype = np.uint64)
     with open(fname, 'rb') as f:
         for i in range(n_frames):
             header[i], raw_data = _read_my302_frame(f, offset, tail, dr)
-            data[i] = ExtractBits(raw_data, dr=dr, bit_nr0=bit_nr0, bit_nr1=bit_nr1)
+            data[i] = ExtractBits(raw_data, dr=dr, bits = bits)
     return header, data
 
 
